375413eec8302c17814800d894d046f2014a5da5
Nehanshu <nehanshugaidhani@gmail.com>
2024-04-01 17:18:06 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Page from './Page';
+import Page2 from './Page2';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Page /> */
+}
+      < Page2/>
+    
+    </div>
+  );
+}
+
+export default App;
